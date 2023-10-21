c87370218d5dfe5d476875d5b4ef0d34eea2a440
JustinVFong <justinvf2018@gmail.com>
2023-10-21 13:32:36 -0700

    Add ObjectToCollect Script
    on start counts total objects left to collect

---
+++ Assets/Scripts/ObjectsToCollect.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsToCollect : MonoBehaviour
+{
+    public static int objects = 0;
+
+    private void Awake(){
+
+        objects++;
+    }
+
+}
