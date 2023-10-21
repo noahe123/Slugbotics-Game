b05b275df39aaeb1792d8c3f5a37128167eccdb4
JustinVFong <justinvf2018@gmail.com>
2023-10-21 13:04:10 -0700

    Add Script
    aa

--- Assets/Scripts/ObjectInteractions.cs
+++ Assets/Scripts/ObjectInteractions.cs
@@ -23,7 +23,7 @@ public class ObjectInteraction : MonoBehaviour
         if (other.gameObject.tag == "Player" && other.gameObject.tag == "Pickup"){
 
             isTouching = true;
-            Debug.Log
+            
         }
     }
 
