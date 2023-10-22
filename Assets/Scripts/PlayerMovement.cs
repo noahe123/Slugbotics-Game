25fbe62da4ccd7ee099e4e7153306401da52cd29
t-amandawong <amandamaenam@gmail.com>
2023-10-22 16:08:42 -0700

    added rotation for roomba movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,10 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f; // Adjust the speed as needed
 
+    public float rotationSpeed = 5f;
+
     private Rigidbody rb;
+
 
     void Start()
     {
@@ -17,6 +20,12 @@ public class PlayerMovement : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed;
+
+        //rotate roomba towards direction
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, -movement, Time.deltaTime * rotationSpeed, 0.0f);
+        transform.rotation = Quaternion.LookRotation(newDirection);
+
+        Debug.DrawRay(transform.position, newDirection, Color.red);
 
         // Apply the movement to the Rigidbody
         rb.velocity = movement;
